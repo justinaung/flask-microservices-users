206aa69d741daa30acd44185b485896c2ba8586d
Justin <justin.aung19@gmail.com>
2017-10-28 23:42:51 +0700

    No need to test DB URIs

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -18,10 +18,6 @@ class TestDevelopmentConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
-        self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_dev'
-        )
 
 
 class TestTestingConfig(TestCase):
@@ -34,10 +30,6 @@ class TestTestingConfig(TestCase):
         self.assertTrue(app.config['DEBUG'])
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
-        self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_test'
-        )
 
 
 class TestProductionConfig(TestCase):
